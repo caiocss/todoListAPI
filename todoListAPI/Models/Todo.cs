5ec85b8d4f1c7e75fd169ee1ab7e0fa7e1ef3f32
Caio Santos <caio.santos687@gmail.com>
2019-08-20 16:25:15 -0300

    create model todo

---
+++ todoListAPI/Models/Todo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace todoListAPI.Models
+{
+    public class Todo
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("Date")]
+        public DateTime Date { get; set; }
+
+        [BsonElement("UserId")]
+        public string UserId { get; set; }
+
+        [BsonElement("Done")]
+        public bool Done { get; set; }
+    }
+}
